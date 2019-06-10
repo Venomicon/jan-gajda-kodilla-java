c03e61f32ac31096b8cda5876628e616852628b8
Venomicon <jangajda9@gmail.com>
2019-06-10 17:23:44 +0200

    Error Gradle

--- calculator/src/main/java/koddilla/course/calculator/Calculator.java
+++ calculator/src/main/java/koddilla/course/calculator/Calculator.java
@@ -1,4 +1,25 @@
 package koddilla.course.calculator;
 
 public class Calculator {
+    int x;
+    int y;
+
+    public Calculator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void add() {
+        System.out.println(x + y);
+    }
+
+    public int subtract() {
+        return x - y;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator(5, 3);
+        calculator.add();
+        calculator.subtract();
+    }
 }
