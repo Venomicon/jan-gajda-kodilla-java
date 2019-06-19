18ae68db70ab018b4dfde481ea8de490fdba1cb6
Venomicon <jangajda9@gmail.com>
2019-06-19 11:20:24 +0200

    Modul 6.2.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator( 5, 3);
+
+        int res1 = calculator.add();
+        int res2 = calculator.subtract();
+
+        if (res1 == (calculator.getA() + calculator.getB())) {
+            if (res2 == (calculator.getA() - calculator.getB())) {
+                System.out.println("Test ok");
+            } else {
+                System.out.println("Error!");
+            }
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
