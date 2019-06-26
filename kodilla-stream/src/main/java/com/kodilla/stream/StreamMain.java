cf61ae2ca26532619fe2319fb15e8ac22136eaba
Venomicon <jangajda9@gmail.com>
2019-06-26 13:44:47 +0200

    Modul 7.4.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,6 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.forumuser.Forum;
-import com.kodilla.stream.forumuser.ForumUser;
+
 
 import java.time.LocalDate;
 import java.util.Map;
@@ -9,17 +8,6 @@ import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-        Forum forum = new Forum();
 
-        Map<Integer, ForumUser> resultMap = forum.getUserList().stream()
-                .filter(n -> n.getSex() == 'M')
-                .filter(n -> (LocalDate.now().getYear() - n.getBirthDate().getYear()) >= 20)
-                .filter(n -> n.getPosts() > 0)
-                .collect(Collectors.toMap(ForumUser::getUserId, user -> user));
-
-        System.out.println("Number of elements: " + resultMap.size());
-        resultMap.entrySet().stream()
-                .map(entry -> entry.getKey() + "  >>>  " + entry.getValue())
-                .forEach(System.out::println);
     }
 }
