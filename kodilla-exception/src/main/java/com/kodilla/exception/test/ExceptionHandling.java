4cb3cbdcf83d75f89b7d8c7bf5d484ffa638c386
Venomicon <jangajda9@gmail.com>
2019-07-02 15:50:45 +0200

    Modul 8.3.

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            secondChallenge.probablyIWillThrowException(1.0,1.5);
+        } catch (Exception e) {
+            System.out.println("Error --->   " + e);
+        } finally {
+            System.out.println("Program finished its work.");
+        }
+    }
+}
